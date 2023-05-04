45858573a958ffc07baf6a17451a47138a7d314f
Lucas Linden <lucas.linden@cwi.com.br>
2023-05-04 10:41:51 -0300

    first commit

---
+++ k6-ebac-tests/requests/login.request.js
@@ -0,0 +1,32 @@
+import http from "k6/http";
+
+export default class Login {
+  // variável privada
+  #token;
+
+  access(user, pass) {
+    let response = http.post(
+      `${Utils.getBaseUrl()}/login`,
+      JSON.stringify(
+        {
+          username: user,
+          password: pass,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+        }
+      )
+    );
+    this.#token = response.json("accessToken");
+    check(request, {
+      "status deve ser 201": (r) => r.status === 201,
+    });
+  }
+
+  getToken() {
+    return this.#token;
+  }
+}
---
+++ k6-ebac-tests/simulations/user.test.js
@@ -0,0 +1,17 @@
+import http from 'k6/http';
+import { group, sleep } from 'k6';
+import data from '../data/usuarios.json'
+
+export default function () {
+  
+  let login = new Login()
+
+  group('login and get token', () => {
+    login.access(data.user, data.pass)
+  })
+
+  group('list users', () => {
+
+  })
+  
+}
